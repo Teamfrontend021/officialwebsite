e889cbfa354e49a731bb0b77405bbc100131611c
Teamfrontend021 <varshini021nexether@gmail.com>
2024-12-06 15:14:21 +0530

    1st commit

--- yashwanth/loading.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded"), () => {
-  const coin = document.querySelector(".coin");
-  const brandName = document.querySelector(".brand-name");
-
-  coin.addEventListener("animationend", () => {
-    brandName.style.opacity = 1;
-  })}
